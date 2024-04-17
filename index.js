30b4621225f8b9672f6217f12fa05c55a261a597
tomtarpeyyt <ttarpey58@gmail.com>
2024-04-17 17:45:58 +0100

    updated index.js with our config

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('time2code - our contact api');
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config/config');
+
+console.log('time2code - with our config in place!');
