0605387fe0b6fbaf3fcb941a9f9be960ceeeb878
tomtarpeyyt <ttarpey58@gmail.com>
2024-04-17 18:14:50 +0100

    updated index.js connecting to MongoDB

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const config = require('./config/config');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// TODO: connect to mongoDB using mongoose
+mongoose.connect(config.dbUrl)
+.then(() => {
+    console.log('connected to MongoDB');
+})
+.catch((err) => {
+    console.error('Error connecting to MongoDB', err);
+});
 
 app.use(express.json());
 
