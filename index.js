f91de18295e8b3fa1081988ce7812967f61abb44
tomtarpeyyt <ttarpey58@gmail.com>
2024-04-17 17:53:55 +0100

    updated index.js with a hello world endpoint

--- index.js
+++ index.js
@@ -3,4 +3,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./config/config');
 
-console.log('time2code - with our config in place!');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// TODO: connect to mongoDB using mongoose
+
+app.use(express.json());
+
+// TODO: ass user and contact routes
+
+// TODO: Remove this endpoint once we have routes
+app.get('/', (req, res) => {
+    res.json("Hello from the backend API...");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
